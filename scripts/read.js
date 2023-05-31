003436f4b9321920839bfe7f0997a91136f41d7d
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2023-05-31 17:23:12 +0530

    Update read.js

--- scripts/read.js
+++ scripts/read.js
@@ -1,9 +1,4 @@
-
-
 const nav = document.querySelector('nav');
-
-
-
 
 function semToggleDropdown() {
     var dropdownMenu = document.getElementById("semDropdownMenu");
@@ -49,3 +44,103 @@ window.matchMedia("(max-width: 800px)").onchange = e => {
   nav.dataset.transitionable = "false";
   nav.dataset.toggled = "false";
 }
+
+
+let data;
+fetchData();
+async function fetchData() {
+  try {
+    const response = await fetch('http://localhost:8080/dog');
+    const obj = (await response.json());
+    data = obj;
+
+    console.log(data[0].sem[0].subjects[0].units[0].topics.length);
+    console.log(data);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+
+
+
+const subjectMobile = document.querySelectorAll('.subject-select-mobile li');
+const subject = document.querySelectorAll('.subject-select  div');
+const unit = document.querySelectorAll('.unit-select li');
+const sem = document.querySelectorAll('.sem-select li');
+const topics = document.querySelector('.topics');
+
+
+const spanSem = document.querySelector(".sem-no");
+const spanUnit = document.querySelector('.unit-no');
+
+let unitNo=0; subNo=0, semesterNo=0;
+
+subject.forEach(sub => {
+  sub.addEventListener('click', event => {
+    subNo = event.target.getAttribute('value');
+    console.log(subNo);
+    showTopics();
+  })
+});
+
+unit.forEach(units => {
+  units.addEventListener('click', event => {
+    unitNo = event.target.getAttribute('value');
+    spanUnit.textContent = `${parseInt(unitNo) + 1}`;
+    showTopics();
+  })
+});
+
+sem.forEach(ssem => {
+  ssem.addEventListener('click', event => {
+    semesterNo = event.target.getAttribute('value');
+    spanSem.textContent = `${parseInt(semesterNo) + 1}`;
+    showTopics();
+  })
+})
+
+function showTopics(){
+  let topicIndex = 0;
+  topics.innerHTML = '';
+  for(let topic of data[0].sem[semesterNo].subjects[subNo].units[unitNo].topics) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('value', topicIndex++);
+
+    const cardImage = document.createElement('div');
+    cardImage.classList.add('card-image');
+    card.appendChild(cardImage);
+
+    const cardText = document.createElement('div');
+    cardText.classList.add('card-text');
+    const cardTextH = document.createElement('h6');
+    cardTextH.textContent = `${topic.name}`;
+    cardText.appendChild(cardTextH);
+
+    card.appendChild(cardText);
+    topics.appendChild(card);
+  }
+
+};
+
+topics.addEventListener('click', async (event) => {
+  console.log(event.target.parentNode)
+  if(event.target.parentNode.classList.contains('card')) {
+    // console.log("mf")
+    let topicNo = event.target.parentNode.getAttribute('value');
+    console.log(topicNo)
+    await fetch('http://localhost:8080/showPost', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify({
+        topicIndex: topicNo,
+        subjectIndex: subNo,
+        unitIndex: unitNo,
+      }) 
+    })
+    window.location.href = 'http://localhost:8080/show';
+  }
+})
