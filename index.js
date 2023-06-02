d069be4824cc023e29427cda1f1e9a3d3ef190d8
Amanjot Singh <98868918+Amanjot-001@users.noreply.github.com>
2023-06-02 10:24:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 let data = '';
-let topicIndex=0, subjectIndex=0, unitIndex=0, content;
+let topicIndex = 0, subjectIndex = 0, unitIndex = 0, content;
 
 app.use("/styles", express.static(__dirname + "/styles"));
 app.use("/scripts", express.static(__dirname + "/scripts"));
@@ -26,80 +26,84 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 const Sem1 = new mongoose.Schema({
-    sem:
-      [
-        {
-          subjects:
-            [{
-              subjectName: String,
-              units:
-                [
-                  {
-                    unitName: String,
-                    topics:
-                      [
-                        {
-                          name: String,
-                          content: String,
-                        },
-                      ]
-                  },
-                ]
-            },
-            ]
-        },
-      ]
-  })
-  
-  const Sem1Notes = mongoose.model('Sem1Notes', Sem1);
-  async function fetchData() {
-    data = await Sem1Notes.find({});
+  sem:
+    [
+      {
+        subjects:
+          [{
+            subjectName: String,
+            units:
+              [
+                {
+                  unitName: String,
+                  topics:
+                    [
+                      {
+                        name: String,
+                        content: String,
+                      },
+                    ]
+                },
+              ]
+          },
+          ]
+      },
+    ]
+})
+
+const Sem1Notes = mongoose.model('Sem1Notes', Sem1);
+async function fetchData() {
+  data = await Sem1Notes.find({});
 }
 
 app.get('/dog', async (req, res) => {
-    res.json(data);
-  })
+  res.json(data);
+})
 
 
 app.get('/read', async (req, res) => {
-    res.render('read', {data});
+  res.render('read', { data });
 })
 
 app.get('/test', (req, res) => {
-    res.render('typing', {content});
+  res.render('typing', { content });
 })
 
 app.post('/topicType', (req, res) => {
-    content = req.body.content;
-    res.sendStatus(200);
+  content = req.body.content;
+  res.sendStatus(200);
 })
 
-app.get('/topicTest', (req,res) => {
-    res.render('typing', {content});
+app.get('/topicTest', (req, res) => {
+  res.render('typing', { content });
 })
 
 app.get('/show', (req, res) => {
-    res.render('show', {topicIndex, subjectIndex, unitIndex, data});
+  res.render('show', { topicIndex, subjectIndex, unitIndex, data });
 })
 
 app.get('/sendNotes', async (req, res) => {
-    res.json({ topicIndex, subjectIndex, unitIndex, data });
+  res.json({ topicIndex, subjectIndex, unitIndex, data });
 })
 
 app.post('/showPost', (req, res) => {
-    topicIndex = req.body.topicIndex;
-    subjectIndex = req.body.subjectIndex;
-    unitIndex = req.body.unitIndex;
-    console.log(topicIndex, unitIndex, subjectIndex);
-    res.sendStatus(200);
+  topicIndex = req.body.topicIndex;
+  subjectIndex = req.body.subjectIndex;
+  unitIndex = req.body.unitIndex;
+  console.log(topicIndex, unitIndex, subjectIndex);
+  res.sendStatus(200);
 })
 
-app.get("*", async(req,res) => {
-    res.sendFile(path.join(__dirname, "./index.html"))
-    if(data == '')
+app.get('/signin', (req, res) => {
+  res.render('sign')
+})
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"))
+  if (data == '')
     await fetchData();
 })
 
 app.listen('8080', () => {
-    console.log('listneing');
+  console.log('listneing');
 })
